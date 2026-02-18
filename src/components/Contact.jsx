2bea3ce5d73f052ba47e9a8bb3859f264f6e6136
Naveen Kumar Vadla <58026402+naveen-kumar-vadla@users.noreply.github.com>
2026-02-18 14:56:06 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -45,7 +45,7 @@ export default function Contact() {
 
                 <div className="flex flex-col sm:flex-row justify-center items-center gap-6 text-gray-800 text-lg">
                     <a
-                        href="https://drive.google.com/file/d/1axr9YRsyYFBjWRC15WztlCzFOIUvN-iL/view?usp=sharing"
+                        href="https://drive.google.com/file/d/15qtXSOgkdob74P_RJO2lW7mlb22iRJ11/view?usp=sharing"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="inline-flex items-center gap-2 px-6 py-3 bg-gray-900 text-white rounded hover:bg-gray-700 mt-4 text-base font-medium"
@@ -54,7 +54,7 @@ export default function Contact() {
                         Resume - Backend Developer
                     </a>
                     <a
-                        href="https://drive.google.com/file/d/1o4fu13YIR2WkSmV5_uhfkfzKKQn5C8jl/view?usp=sharing"
+                        href="https://drive.google.com/file/d/1y7X5ytSBUs75U1aLsirQgZAaeqwiZA5p/view?usp=sharing"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="inline-flex items-center gap-2 px-6 py-3 bg-gray-900 text-white rounded hover:bg-gray-700 mt-4 text-base font-medium"
